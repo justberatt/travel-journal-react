2f192a3ac2d939d35a006de41fda02367e374781
Berat Sakipi <berat.sakipi@gmail.com>
2025-01-09 00:18:36 +0100

    some commects added, to make more sense of the things I have written and the things I could have written

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ export default function App() {
         //   alt: entry.img.alt
         // }}
         // But since the Entry component is expecting the 'img' prop to be an object, and is expecting that object to have
-        // the 'src' property and the 'alt' property
+        // the 'src' property and the 'alt' property, and the data has exactly that, we can just write: 'img={entry.img}',
+        // as we have done below
         img={entry.img}
         title={entry.title}
         country={entry.country}
@@ -21,6 +22,23 @@ export default function App() {
         dates={entry.dates}
         text={entry.text}
       />
+      // You can also just do this:
+      // <Entry
+      //     key={entry.id}
+      //     entry={entry}
+      // />
+      // And it would require to go to the Entry component and access the props with: props.entry.whatever.
+      // This is an even more concise way, but the key names of the key-value pairs in the data file should match the names of the keys
+      // in the Entry component. So if in the data the key for image is img, in the Entry component you should access it
+      // as props.entry.src.img, you cannot say props.entry.src.image. This is the only reason why we can use this concise way
+
+      // We can also use:
+      // <Entry
+      //     key={entry.id}
+      //     {...entry}
+      // />
+      // It will spread all the properties, and we won't have to use props.entry.whatever in the Header comopnent,
+      // we would simply have to use props.whatever.
     )
   })
     return (
